fc7fa9dd0ff320980dd723dc8436991cd09fec05
Maromitaz <50496210+Maromitaz@users.noreply.github.com>
2021-11-21 18:34:50 +0200

    .

--- test.py
+++
@@ -1,6 +0,0 @@
-import framework
-color_import = framework.Color.custom_color
-custom_colors = color_import("DarkSlateGray", (47,79,79))
-color_import = custom_colors("Red", (255,0,0))
-
-print(custom_colors)
